05309cf89a9bff6da4f0cb98bb554fc463cda852
ycas2002 <ycas2002@yahoo.com>
2018-12-14 17:27:52 +0100

    za slanje

--- js/menu.js
+++ js/menu.js
@@ -3,17 +3,32 @@
 //     $('#logo').css({ transform: 'rotate(' + theta + 'rad)' });
 // });
 var lastScrollTop = 0;
+$(document).ready(function(event){
+  var url = window.location.pathname.split("/", 3);
+  if(url[2] == "contact") {
+    $(".logo").css('background-image', 'url(./Images/logow.svg)');
+  }
+});
 $(window).scroll(function(event){
   var st = $(this).scrollTop();
+  var url = window.location.pathname.split("/", 3);
   if (st < 100) {
     // $( ".header" ).removeClass('header--closed').addClass('header--opened');
     if( st < 60 ){
       $( ".logo" ).removeClass("logo--letter");
-      $( ".logo" ).css("background-image", "url('images/logo.svg')");
+      if(url[2] == "contact") {
+        $(".logo").css('background-image', 'url(./Images/logow.svg)');
+      } else {
+        $( ".logo" ).css("background-image", "url('images/logo.svg')");
+      }
       $('.logo').css({ transform: 'rotate(0rad)' });
     } else {
       $( ".logo" ).addClass("logo--letter");
-      $( ".logo" ).css("background-image", "url('images/logo-c.svg')");
+      if(url[2] == "contact") {
+        $(".logo").css('background-image', 'url(./Images/logo-cw.svg)');
+      } else {
+        $( ".logo" ).css("background-image", "url('images/logo-c.svg')");
+      }
     }
     return; }
   if (st > lastScrollTop){
@@ -26,11 +41,19 @@ $(window).scroll(function(event){
     // $( ".header" ).removeClass('header--closed').addClass('header--opened');
     if( st < 100 ){
       $( ".logo" ).removeClass("logo--letter");
-      $( ".logo" ).css("background-image", "url('images/logo.svg')");
+      if(url[2] == "contact") {
+        $(".logo").css('background-image', 'url(./Images/logow.svg)');
+      } else{
+        $( ".logo" ).css("background-image", "url('images/logo.svg')");
+      }
       $('.logo').css({ transform: 'rotate(0rad)' });
     } else {
       $( ".logo" ).addClass("logo--letter");
-      $( ".logo" ).css("background-image", "url('images/logo-c.svg')");
+      if(url[2] == "contact") {
+        $(".logo").css('background-image', 'url(./Images/logo-cw.svg)');
+      } else {
+        $( ".logo" ).css("background-image", "url('images/logo-c.svg')");
+      }
       var theta = st / 60 % Math.PI;
       $('.logo').css({ transform: 'rotate(' + theta + 'rad)' });
     }
