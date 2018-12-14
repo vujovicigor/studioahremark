35b02aee6190c0a1a4be5c26f2c0621dca5c5980
ycas2002 <ycas2002@yahoo.com>
2018-12-14 12:36:57 +0100

    footer heder i jos po nsto

--- js/menu.js
+++ js/menu.js
@@ -5,29 +5,32 @@
 var lastScrollTop = 0;
 $(window).scroll(function(event){
   var st = $(this).scrollTop();
-  console.log(st);
   if (st < 100) {
-    $( ".header" ).removeClass('header--closed').addClass('header--opened');
-    if( st < 100 ){
+    // $( ".header" ).removeClass('header--closed').addClass('header--opened');
+    if( st < 60 ){
+      $( ".logo" ).removeClass("logo--letter");
       $( ".logo" ).css("background-image", "url('images/logo.svg')");
       $('.logo').css({ transform: 'rotate(0rad)' });
     } else {
+      $( ".logo" ).addClass("logo--letter");
       $( ".logo" ).css("background-image", "url('images/logo-c.svg')");
-     
     }
     return; }
   if (st > lastScrollTop){
     // downscroll code
-    $( ".header" ).removeClass('header--opened').addClass('header--closed');
+    // $( ".header" ).removeClass('header--opened').addClass('header--closed');
+    var theta = st / 60 % Math.PI;
+    $('.logo').css({ transform: 'rotate(' + theta + 'rad)' });
   } else {
     // upscroll code
-    $( ".header" ).removeClass('header--closed').addClass('header--opened');
+    // $( ".header" ).removeClass('header--closed').addClass('header--opened');
     if( st < 100 ){
+      $( ".logo" ).removeClass("logo--letter");
       $( ".logo" ).css("background-image", "url('images/logo.svg')");
       $('.logo').css({ transform: 'rotate(0rad)' });
     } else {
+      $( ".logo" ).addClass("logo--letter");
       $( ".logo" ).css("background-image", "url('images/logo-c.svg')");
-      console.log("hcak");
       var theta = st / 60 % Math.PI;
       $('.logo').css({ transform: 'rotate(' + theta + 'rad)' });
     }
