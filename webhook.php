5dafeb82be0919bea60ce361e7495a77b95f0997
vujovicigor <vujovichigor@gmail.com>
2018-12-27 14:35:52 +0100

    wh test 5

--- webhook.php
+++ webhook.php
@@ -1,10 +1,10 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_POST['payload'] ) {
+//if ( $_POST['payload'] ) {
  // shell_exec( 'git reset --hard HEAD && git pull' );
   //echo shell_exec( 'git reset --hard HEAD' ); 
   echo shell_exec( 'git pull' );
-  echo shell_exec( 'git status' );
+  //echo shell_exec( 'git status' );
   //print_r($_POST);
-}
+//}
 ?>hi....
