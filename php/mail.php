368c8a63168a11186e1b260dd59c2bd9507072d6
igor <igor@IgorBuildconMBP.local>
2019-03-22 14:27:55 +0100

    mail 2

--- php/mail.php
+++ php/mail.php
@@ -1,5 +1,8 @@
 <?php
     header('Content-Type: application/json');
+
+
+    // MEJL VLASNIKU
     $to = "info@credo.agency";
     $from = $_POST["email"];
     $imeprezime = $_POST["name"];
@@ -51,6 +54,66 @@
     //var_dump($response,curl_error($session),curl_getinfo($session));
 
     curl_close($session);
+
+
+    
+
+    // MEJL KLIJENTU 
+    $to = $_POST["email"];
+    $from = "info@credo.agency";
+    $imeprezime = $_POST["name"];
+    $subject = 'Credo'; // PItaj Iliju koji subject oce
+    $message = <<<TEXT
+Dear $imeprezime,
+
+Thank you for showing interest for our services. We will get back to you as soon as possible.
+
+Best regards,
+CREDO
+TEXT;
+
+    $url = 'https://api.sendgrid.com/';
+    $user = 'agency';
+    $pass = 'kuwait2019';
+
+    $params = array(
+        'api_user'  => 'credo'.$user,
+        'api_key'   => 'credo'.$pass,
+        'to'        => $to,
+        'subject'   => $subject,
+        //'html'      => 'html body',
+        'text'      => $message,
+        'from'      => $from,
+      );
+
+    $request =  $url.'api/mail.send.json';
+
+    // Generate curl request
+    $session = curl_init($request);
+    // Tell curl to use HTTP POST
+    curl_setopt ($session, CURLOPT_POST, true);
+    // Tell curl that this is the body of the POST
+    curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+    // Tell curl not to return headers, but do return the response
+    curl_setopt($session, CURLOPT_HEADER, false);
+    // Tell PHP not to use SSLv3 (instead opting for TLS)
+    //curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+
+    //Turn off SSL
+    curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);//New line
+    curl_setopt($session, CURLOPT_SSL_VERIFYHOST, false);//New line
+
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+    // obtain response
+    $response = curl_exec($session);
+
+    // print everything out
+    //var_dump($response,curl_error($session),curl_getinfo($session));
+
+    curl_close($session);
+
+
 
     echo json_encode(array("Message" => "Message succesfuly sent, we'll get back to you soon.", "status" => true ));
 
