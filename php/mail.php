cb5a7d32bcc843ccbf13ea7915dd7a6cb104b3cd
igor <igor@IgorBuildconMBP.local>
2019-03-22 10:21:55 +0100

    mail

--- php/mail.php
+++ php/mail.php
@@ -8,12 +8,61 @@
     $subject = $_POST["subject"];
     $message = $_POST["message"];
     $message = "Poruka od:" .$imeprezime. "\r\n" .$message;
-    $headers = "From: $imeprezime <$to>"."\r\n"."Reply-to: $from"."\r\n"."X-Mailer: PHP/".phpversion();
+    //$headers = "From: $imeprezime <$to>"."\r\n"."Reply-to: $from"."\r\n"."X-Mailer: PHP/".phpversion();
 
+
+
+    $url = 'https://api.sendgrid.com/';
+    $user = 'agency';
+    $pass = 'kuwait2019';
+    
+    $params = array(
+        'api_user'  => 'credo'.$user,
+        'api_key'   => 'credo'.$pass,
+        'to'        => $to,
+        'subject'   => $subject,
+        //'html'      => 'html body',
+        'text'      => $message,
+        'from'      => 'info@credo.agency',
+      );
+    
+    $request =  $url.'api/mail.send.json';
+    
+    // Generate curl request
+    $session = curl_init($request);
+    // Tell curl to use HTTP POST
+    curl_setopt ($session, CURLOPT_POST, true);
+    // Tell curl that this is the body of the POST
+    curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+    // Tell curl not to return headers, but do return the response
+    curl_setopt($session, CURLOPT_HEADER, false);
+    // Tell PHP not to use SSLv3 (instead opting for TLS)
+    //curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+    
+    //Turn off SSL
+    curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);//New line
+    curl_setopt($session, CURLOPT_SSL_VERIFYHOST, false);//New line
+    
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+    
+    // obtain response
+    $response = curl_exec($session);
+    
+    // print everything out
+    //var_dump($response,curl_error($session),curl_getinfo($session));
+    
+    curl_close($session);
+
+    echo json_encode(array("Message" => "Message succesfuly sent, we'll get back to you soon.", "status" => true ));
+
+
+
+
+/*
     if(mail($to, $subject, $message, $headers)) {
       echo json_encode(array("Message" => "Message succesfuly sent, we'll get back to you soon.", "status" => true ));
     } else {
       echo json_encode(array("Message" => "Error sending message, please try later.", "status" => false ));
     }
-
+*/
   ?>
